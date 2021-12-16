3793f552057de5350c5bef6524e3b8db239e347b
harunozceyhan <harunozceyhan@gmail.com>
2021-12-16 14:48:54 +0300

    create plusauth.js

---
+++ plusauth.js
@@ -0,0 +1,11 @@
+import { OIDCClient } from '@plusauth/plusauth-oidc-client-js';
+
+const oidcClient = new OIDCClient({
+  issuer: 'YOUR_OIDC_PROVIDER',
+  client_id: 'YOUR_CLIENT_ID',
+  redirect_uri: 'YOUR_CALLBACK_PAGE_URI'
+});
+
+oidcClient.initialize().then( function(){
+  // client initialized
+})
