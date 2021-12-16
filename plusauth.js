507386a95fb28893f60b1b0f6221504a04905906
harunozceyhan <harunozceyhan@gmail.com>
2021-12-16 14:57:53 +0300

    Update plusauth.js

--- plusauth.js
+++ plusauth.js
@@ -1,13 +1,19 @@
 import { OIDCClient } from '@plusauth/plusauth-oidc-client-js';
 
-const oidcClient = new OIDCClient({
-  issuer: 'YOUR_OIDC_PROVIDER',
-  client_id: 'YOUR_CLIENT_ID',
-  redirect_uri: 'YOUR_CALLBACK_PAGE_URI'
-});
 
-oidcClient.initialize().then( function(){
-  // client initialized
-});
 
-alert('sd')
+initialize() {
+  withPluginApi("0.8.20", (api) => {
+    const oidcClient = new OIDCClient({
+      issuer: 'YOUR_OIDC_PROVIDER',
+      client_id: 'YOUR_CLIENT_ID',
+      redirect_uri: 'YOUR_CALLBACK_PAGE_URI'
+    });
+
+    oidcClient.initialize().then( function(){
+      // client initialized
+    });
+
+    alert('sd');
+  }
+}
